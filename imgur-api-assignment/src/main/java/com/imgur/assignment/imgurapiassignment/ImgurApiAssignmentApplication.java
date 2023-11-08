c10e08b5392058967a6a872443e64f5b91a3212e
wassubaba <waseem.cmc@gmail.com>
2023-11-08 14:09:27 +0530

    Imgur Assignment Initial CheckIn
    Imgur Assignment Initial CheckIn

---
+++ imgur-api-assignment/src/main/java/com/imgur/assignment/imgurapiassignment/ImgurApiAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.imgur.assignment.imgurapiassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgurApiAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgurApiAssignmentApplication.class, args);
+	}
+
+}
