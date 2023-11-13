b4ca2af4e9b2ce65bfc8c668d6cfb6a952322d1d
wassubaba <waseem.cmc@gmail.com>
2023-11-13 14:18:37 +0530

    Updates
    Updates

--- imgur-api-assignment/src/main/java/com/imgur/assignment/imgurapiassignment/config/SecurityConfig.java
+++ imgur-api-assignment/src/main/java/com/imgur/assignment/imgurapiassignment/config/SecurityConfig.java
@@ -28,7 +28,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	  }
 	  
 	  @Override
-	   protected void configure(HttpSecurity http) throws Exception {
-	      http.csrf().disable();
+	   protected void configure(HttpSecurity httpSecurity) throws Exception {
+		  
+		  httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
+          .authorizeRequests().antMatchers("/console/**").permitAll();
+		  httpSecurity.csrf().disable();
+		  httpSecurity.headers().frameOptions().disable();
+	      
 	   }
 }
